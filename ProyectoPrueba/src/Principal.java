d7a98572a0c8dc9fb973b8cee9299e72348c5933
francentelles <francentelles05@gmail.com>
2024-12-19 16:12:39 +0100

    Incluyo una forma ingeniosa de despedirse
    Chao bacalao!

--- ProyectoPrueba/src/Principal.java
+++ ProyectoPrueba/src/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Agur Petisuis!");
+		System.out.println("Chao bacalao!");
 
 	}
 
